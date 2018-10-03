233f48bc242ad4dd21ed68aee9d6e944284f1113
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-03 03:42:07 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status, :transfered
+
+   def initialize(sender, receiver, amount)
+     @sender, @receiver, @amount, @status, @transfered = sender, receiver, amount, "pending", true
+   end
+
+   def valid?
+     sender.valid? and receiver.valid?
+   end
+
+   def execute_transaction
+     if valid? and transfered and sender.balance >= amount
+       receiver.balance += amount
+       sender.balance -= amount
+       @status, @transfered = "complete", false
+     else
+       @status = "rejected"
+       "Transaction rejected. Please check your account balance."
+     end
+   end
+
+   def reverse_transfer
+     if !transfered
+       sender.balance += amount
+       receiver.balance -= amount
+       @status, @transfered = "reversed", true
+     end
+   end
 end
