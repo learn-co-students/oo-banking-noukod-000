c0c3c6693afec232cdaaa200798565f435960c47
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-26 17:52:05 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,10 @@
 class BankAccount
-
+  attr_accessor :name, :balance, :status
+  
+  
+  def initialize(name, balance=1000, status="open")
+    @name=name
+    @balance = balance
+    @status=status
+  end
 end
